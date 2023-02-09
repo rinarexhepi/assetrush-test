ce6305942271f7a9b697e082d664e88ae55a2294
rinarexhepi <rinnaarexhepi@gmail.com>
2023-02-09 10:31:51 +0100

    Small Changes

--- components/Grids.vue
+++ components/Grids.vue
@@ -5,6 +5,16 @@
 </template>
   
   <script lang="ts">
+  const variantClasses  = {
+          'fullBleed': 'lg:grid-cols-12 gap-4 md:grid-cols-6 sm:grid-cols-6 bg-slate-300 py-6',
+          'guttersOnly': 'lg:grid-cols-12 gap-4 md:grid-cols-6 px-10 bg-slate-400 py-6',
+          'halfAndHalf': 'lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 w-full bg-slate-500 py-6',
+          'half': 'grid grid-cols-6 ',
+          'standard': 'lg:grid-cols-12 gap-4 md:grid-cols-6 sm:grid-cols-6 xs:grid-cols-6 bg-white',
+          'centered': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 gap-4 mx-20 bg-indigo-400 py-6',
+          'cards': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 gap-4 mx-20 bg-indigo-400 py-6',
+          'centeredCards': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 xs:grid-cols-6 gap-4 justify-center',
+        }
    import { PropType } from 'vue'
    import { GridVariant } from '~/types/types'
     export default {
@@ -14,16 +24,7 @@
     },
   },
     setup(props) {
-      const variantClasses  = {
-          'fullBleed': 'lg:grid-cols-12 gap-4 md:grid-cols-6 sm:grid-cols-6 bg-slate-300 py-6',
-          'guttersOnly': 'lg:grid-cols-12 gap-4 md:grid-cols-6 px-10 bg-slate-400 py-6',
-          'halfAndHalf': 'lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 w-full bg-slate-500 py-6',
-          'half': 'grid grid-cols-6 ',
-          'standard': 'lg:grid-cols-12 gap-4 md:grid-cols-6 sm:grid-cols-6 xs:grid-cols-6 bg-white',
-          'centered': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 gap-4 mx-20 bg-indigo-400 py-6',
-          'cards': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 gap-4 mx-20 bg-indigo-400 py-6',
-          'centeredCards': 'lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-6 xs:grid-cols-6 gap-4 justify-center'
-        }
+      
         const useVariantClasses = (variant: GridVariant) => {
           return variantClasses[variant];
         }
