106ce47f88f2912a44a35d0913b12b40ea352762
rinarexhepi <rinnaarexhepi@gmail.com>
2023-03-08 11:40:09 +0100

    Number Position Refix

--- components/VideoCard/VideoCard.vue
+++ components/VideoCard/VideoCard.vue
@@ -7,9 +7,9 @@
       </span>
     </template>
 
-    <div v-if="number" class="bottom-2 -left-8 absolute z-20 float-right">
+    <div v-if="number" class="bottom-5 -left-5 absolute z-20 float-right">
       <span
-        class="text-outline font-gtplanar semibold lg:text-9xl md:text-8xl sm:text-8xl xs:text-8xl xxs:text-7xl"
+        class="text-outline font-gt-planar semibold lg:text-9xl md:text-8xl sm:text-8xl xs:text-8xl xxs:text-7xl"
       >
         {{ number }}
       </span>
