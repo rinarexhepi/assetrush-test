0f7da7d52a3a024a7934589277b4484fce9b6815
rinarexhepi <rinnaarexhepi@gmail.com>
2023-03-09 09:48:45 +0100

    Episode Card Fix

--- components/VideoCard/EpisodeCard.vue
+++ components/VideoCard/EpisodeCard.vue
@@ -17,9 +17,9 @@
         class="m-4 p-4 flex lg:flex-col sm:flex-col-reverse flex-col-reverse"
       >
         <div
-          class="flex lg:flex-row md:flex-col sm:flex-col flex-col-reverse justify-between "
+          class="flex lg:flex-row md:flex-col sm:flex-col flex-col justify-between "
         >
-          <div class="z-20">
+          <div class="z-20 py-2">
             <h3 class="uppercase text-xl font-semibold mr-6 w-auto">
               Episode {{ episodeNumber }}
             </h3>
