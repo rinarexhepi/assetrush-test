1d4cfab8afdd7361bd5e9b38ab50d576c8ff0f69
rinarexhepi <rinnaarexhepi@gmail.com>
2023-03-17 16:51:14 +0100

    Added Transition to Third Button on hover

--- components/Info/Newsletter.vue
+++ components/Info/Newsletter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="py-10 flex flex-row justify-center gap-8 w-full">
+  <div class="py-10 flex lg:flex-row flex-col justify-center gap-8 w-full">
     <div class="flex flex-col gap-8">
-      <div class=" flex flex-col w-full">
+      <div class=" flex flex-col lg:items-start items-center w-full">
         <Headline level="h6" headingVariant="subtitles"
           >AssetRush pulse newsletter
         </Headline>
@@ -9,23 +9,23 @@
           stay in the loop with our monthly newsletter
         </p>
       </div>
-      <div class="flex lg:flex-row flex-col gap-2">
+      <div class="flex lg:flex-row flex-col items-center gap-2">
         <input
           type="text"
           placeholder="Your Email Address"
           name="email"
           id=""
-          class="rounded-full border-2 py-3 px-7 border-slate-600 bg-transparent"
+          class="rounded-full border-2 py-3 px-7 border-slate-600 bg-transparent w-max"
         />
         <StrapiButton buttonVariant="primary" text="Subscribe" class="w-max" />
       </div>
     </div>
     <div class=" flex  lg:flex-row flex-col items-center gap-4  ">
-      <Headline level="h5" headingVariant="pastARPresenters"
-        >Asset Rush on Youtube</Headline
-      >
+      <Headline level="h5" headingVariant="pastARPresenters">
+        Asset Rush on Youtube
+      </Headline>
       <img
-        class="flex shrink-0  h-[65px]"
+        class="flex shrink-0 h-[40px]"
         src="~/assets/img/youtube.png"
         alt="youtube"
       />
--- components/StrapiButton/const.ts
+++ components/StrapiButton/const.ts
@@ -3,7 +3,8 @@ export const buttonClasses = {
     "relative w-full h-full uppercase border-2 border-transparent hover:border-slate-200 ease-in duration-300 rounded-full text-white bg-white/20 mx-2 py-2 px-4 ",
   secondary:
     "uppercase bg-blue-600 hover:bg-blue-500 scale-100 hover:scale-105 transition duration-300 ease-in-out text-white rounded-full",
-  third: " uppercase text-blue-600 bg-white rounded-full",
+  third:
+    " uppercase text-blue-600 bg-white hover:bg-blue-600 hover:text-white transition ease-in-out duration-300 rounded-full",
   primaryCornered:
-    "uppercase hover:bg-white/40  transition duration-300 ease-in-out text-white bg-white/20 py-2 px-4 ",
+    "uppercase hover:bg-white/40  transition duration-500 ease-in-out text-white bg-white/20 py-2 px-4 ",
 };
